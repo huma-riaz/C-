5b9ed6f45cc5adad5587791077ed72abdfc23286
Huma Riaz <riazhuma2005@gmail.com>
2025-03-18 23:35:30 +0500

    Create Program01.cs

---
+++ Program01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone!");
+            Console.Write("I'm Huma ");
+            Console.WriteLine("This is my First File ");
+            Console.ReadLine();
+        }
+    }
+}
