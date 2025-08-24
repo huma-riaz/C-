54cd948ccfe4ed7d1f11d3206e0a34aae17b34fa
Huma Riaz <riazhuma2005@gmail.com>
2025-08-24 18:16:58 +0500

    Edited

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat mycat = new Cat();
+            mycat.name = "Mano ";
+            mycat.sound = "Meoww Meoww";
+            mycat.Details();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public string name;
+        public string sound;
+    }
+    class Dog : Animal
+    {
+
+    }
+    class Cat : Dog
+    {
+        public void Details()
+        {
+            Console.WriteLine("Animal name is {0}, Sound is {1}", 
+                            this.name,this.sound);
+        }
+    }
+
+
+}
