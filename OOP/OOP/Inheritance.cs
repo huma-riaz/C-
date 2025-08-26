63a107e6d8feb6f39695b3c521910ce3c94b2bc5
Huma Riaz <riazhuma2005@gmail.com>
2025-08-26 23:05:54 +0500

    Edited

--- OOP/OOP/Inheritance.cs
+++ OOP/OOP/Inheritance.cs
@@ -11,4 +11,23 @@ namespace OOP
     {
         public string name;
     }
-    
+    class Cat : Animals    //Child Class (Inherit)
+    {
+        public string sound;
+        public void Details()
+        {
+            Console.WriteLine("Animal name is {0} And sound is {1}", this.name, this.sound);
+        }
+    }
+    class Dog : Cat      //Multi Level Class
+    {
+        public void dogDetails()
+        {
+            Console.WriteLine("Animal name is {0} And sound is {1}", this.name, this.sound);
+        }
+    }
+
+
+
+
+}
