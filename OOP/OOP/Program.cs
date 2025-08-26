ecc38f68d0d06058d940596f362dbe5ed7b99122
Huma Riaz <riazhuma2005@gmail.com>
2025-08-26 23:02:58 +0500

    Edited

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object For Inheritance:
+            Cat mycat = new Cat();
+            mycat.name = "Mano";
+            mycat.sound = "Meoww Meoww";
+            mycat.Details();
+
+            Dog mydog = new Dog();
+            mydog.name = "Coco";
+            mydog.sound = "Bhaoo Bhaaooo";
+            mydog.dogDetails();
+
+
+            Console.ReadKey();
+        }
+    }
+}
