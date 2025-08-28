5b4b8bbb135c4b23879e70afbabadab8e5041d85
Huma Riaz <riazhuma2005@gmail.com>
2025-08-28 22:54:50 +0500

    Edited

--- OOP/OOP/Polymorphism.cs
+++ OOP/OOP/Polymorphism.cs
@@ -7,4 +7,47 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
-    
+    // Many Types: 
+    // Compile time polymorphism (same method, different parameters )
+    class Calculator
+    {
+        public void calculate(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Calculate(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public void Calculate(int a, int b, int c)
+        {
+            Console.WriteLine(a * b * c);
+        }
+
+    }
+
+
+    // Run time polymorphism (Parent class ko override kr k child class apna behavior show krta hai)
+    class Student
+    {
+        virtual public void name()
+        {
+            Console.WriteLine(" My name is Abdul Rafay");
+        }
+    }
+    class std : Student
+    {
+        override public void name()
+        {
+            Console.WriteLine(" My name is Huma ");
+        }
+    }
+    class newstd : Student
+    {
+        override public void name()
+        {
+            Console.WriteLine(" My name is Huma Riaz");
+        }
+    }
+
+}
