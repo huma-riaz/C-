2fb28043a22fc6a3aeccf26227f0e7829d8aa2ad
Huma Riaz <riazhuma2005@gmail.com>
2025-09-22 21:09:44 +0500

    Edited

---
+++ Generics/Generics/Generic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+  //  Generic type-safety aur reusability k liye use krty hn...
+
+    //   GENERIC CLASS:
+    public class Box<T>
+    {
+        public T Value { get; set; }
+    }
+
+    //   NON-GENERIC CLASS:
+
+
+
+}
---
+++ Generics/Generics/NameSpace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+  //  Aik he class different namespace mai...
+    class Student
+    {
+        public void sname1()
+        {
+            Console.WriteLine("First Namespace ");
+        }
+
+    }
+}
+
+namespace namespace2
+{
+    class Student
+    {
+        public void sname2()
+        {
+            Console.WriteLine("Second Namespace ");
+        }
+    }
+    
+}
